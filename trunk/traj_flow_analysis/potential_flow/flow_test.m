27e01042ed7563024d39ac0f00c5c3e9967dd7d0
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2011-11-12 01:32:43 +0000

    Added: projection functions for SCF

--- trunk/traj_flow_analysis/potential_flow/flow_test.m
+++ trunk/traj_flow_analysis/potential_flow/flow_test.m
@@ -1,24 +1,24 @@
 flat = @(x) x(:);
-
-%%
 num_var = 20;
+nbins = 25;
+Cvv = zeros(num_var,nbins);
 for i = 1:num_var
     
     [W0,x,y] = power_law(1,pi/4,1,-10:1:10);
-    W1 = source_sink_swirl(i*.1i,0,-10:1:10);
+    W1 = source_sink_swirl(i*5i,0,-10:1:10);
     W2 = source_sink_swirl(i*1,0,-10:1:10);
-    W3 = source_sink_swirl(-1,1-5i,-10:1:10);
+    W3 = source_sink_swirl(i*-1,0,-10:1:10);
     W4 = doublet(.1*i,0);
     W5 = vortex(10,1*i,-1+1i,-10:1:10);
     
     W = W0 + W4;
     [Vx,Vy] = velocity_from_potential(W);
-    if any(i == [1,20])
-        switch i, case 1, n = 1; str = 'low'; case 20, n = 2; str = 'high'; end
+    if any(i == [1,10])
+        switch i, case 1, n = 1; str = 'low'; case 10, n = 2; str = 'high'; end
         figure(3)
         h(n) = subplot(1,2,n);
         quiver(Vx,Vy,0), axis equal, axis tight
-        title([str ' power.'])
+        title([str ' swirl.'])
         %         figure(2)
         %         pcolor(x,y,imag(W))
     end
@@ -26,7 +26,7 @@ for i = 1:num_var
     V = cat(3,Vx,Vy);
     V = grid2list(flat(V(:,:,1)),flat(V(:,:,2)));
     centroids = grid2list(x(:),y(:));
-    [Cvv(i,:),R] = spatial_correlation_function(V,centroids,30);
+    [Cvv(i,:),R] = spatial_correlation_function(V,centroids,nbins,'off','on');
 end
 
 linkaxes(h)
@@ -34,7 +34,7 @@ figure
 [foo,bar] = meshgrid(R,1:num_var);
 pcolor(foo,bar,Cvv),caxis([-1 1]); colorbar
 xlabel('Distance (R)')
-ylabel('Power')
+ylabel('Swirl size')
 
 %%
 
@@ -45,10 +45,11 @@ flat = @(x) x(:);
 x = -20:1:20;
 zA = [0];
 
-[W0,X,Y] = power_law(1,pi/4,1,x);
-W1 = source_sink_swirl(i,zA);
+[W0,X,Y] = power_law(1,pi/2,1,x);
+W1 = source_sink_swirl(1i,zA,x);
+W2 = doublet(10,zA,x);
 
-W = W0;
+W = W0 + W1;
 contour(X,Y,imag(W)),axis equal;
 [Vx,Vy] = velocity_from_potential(W);
 figure,quiver(Vx,Vy,0),axis equal
@@ -56,5 +57,5 @@ figure,quiver(Vx,Vy,0),axis equal
 V = cat(3,Vx,Vy);
 V = grid2list(flat(V(:,:,1)),flat(V(:,:,2)));
 centroids = grid2list(X(:),Y(:));
-[Cvv,R] = spatial_correlation_function(V,centroids,26);
+[Cvv,R] = spatial_correlation_function(V,centroids,25,'off');
 figure,plot(R,Cvv);
--- trunk/traj_flow_analysis/spatial_correlation_function.m
+++ trunk/traj_flow_analysis/spatial_correlation_function.m
@@ -1,31 +1,55 @@
-function [Cvv,R] = spatial_correlation_function(X,centroids,nbins)
+function [Cvv,R] = spatial_correlation_function(V,centroids,nbins,local,mean_subt)
 
-N = size(X,1);
+if ~exist('local','var'), local = 'off'; end
+if ~exist('mean_subt','var'), mean_subt = 'off'; end
+
+N = size(V,1);
 distances = pdist(centroids);
-% min_d = min(distances);
+min_d = min(distances);
 max_d = max(distances);
-R = linspace(0,max_d,nbins);
+R = linspace(min_d,max_d,nbins-1);
+edges = [-inf R];
 
-[counts,which_bins] = histc(distances,R);
+[counts,which_bins] = histc(distances,edges);
 Cvv = zeros(1,nbins);
 which_bins = squareform(which_bins);
+which_bins(logical(eye(N))) = 1;
 
-projections = pdist(V,@project_vectors);
+counts = counts*2;
+
+V(isinf(V)) = NaN;
+if strcmpi(mean_subt,'on')
+    means = nanmean(V,1);
+    V = V - means(ones(1,N),:);
+end
+
+if strcmpi(local,'off')
+    projections = pdist(V,@project_vectors);
+    norms = sqrt(sum(V.^2,2));
+%     norms(isinf(norms)) = NaN;
+else
+    projections = pdist(V,@project_vectors_norm);
+end
 projections = squareform(projections);
-projections(logical(eye(N))) = 1;
 
-norms = sqrt(sum(V.^2,2));
-norms(isinf(norms)) = NaN;
-
+% projections(logical(eye(N))) = norms.^2;
 
 for i = 1:nbins
     members = projections(which_bins == i);
-    members(isinf(members)) = NaN;
+%     members(isinf(members)) = NaN;
     Cvv(i) = nansum(members);
 end
-Cvv = Cvv./counts;
 
+if strcmpi(local,'off')
+    Cvv = (Cvv./counts)/nanmean(norms.^2);
+else
+    Cvv = Cvv./counts;
+end
+
+Cvv(1) = 1;
+R = [0 R];
 %Add in the 0 distance terms
 % Cvv = [1 Cvv];
 % R = [0 R];
+
 end
