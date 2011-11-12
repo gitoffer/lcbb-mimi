9cdc5dcdf7fb689fb48d96ad5df6c736515a8a3a
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2011-11-12 22:59:32 +0000

    Fixed options argument for SCF/STICS

--- trunk/traj_flow_analysis/get_scf4stics.m
+++ trunk/traj_flow_analysis/get_scf4stics.m
@@ -1,4 +1,8 @@
-function [C,R] = get_scf4stics(stics_img,Xf,Yf,stics_o,time_avg)
+function [C,R] = get_scf4stics(stics_img,Xf,Yf,stics_o,options)
+
+time_avg = options{1};
+local_norm = options{2};
+mean_subtract = options{3};
 
 flat = @(x) x(:);
 T = numel(stics_img);
@@ -11,7 +15,7 @@ for i = 1:T
     V = stics_img{i};
     V = grid2list(flat(V(:,:,1)),flat(V(:,:,2)));
     tic
-    [C(i,:),R] = spatial_correlation_function(V,centroids,nbins,'on','off');
+    [C(i,:),R] = spatial_correlation_function(V,centroids,nbins,local_norm,mean_subtract);
     toc
 end
 
