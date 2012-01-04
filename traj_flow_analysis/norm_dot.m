836002ecc581c798f242bcf3f495e1bd8bc965f2
xie.shicong <xie.shicong@gmail.com>
2012-01-04 19:21:43 +0000

    Changed traj_flow_analysi file

--- traj_flow_analysis/norm_dot.m
+++ traj_flow_analysis/norm_dot.m
@@ -1,5 +1,8 @@
 function dist = norm_dot(Xi,Xj)
-
+%NORM_DOT Used as a 'distance metric' for calculating SCFs.
+% To be used by pdist, and not by itself.
+%
+% xies@mit. Nov 2010.
 
 N = size(Xj,1);
 
