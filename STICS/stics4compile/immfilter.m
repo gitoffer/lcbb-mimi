bfb8eefd636025b17492c025bb49bd0da8d11b2c
xie.shicong <xie.shicong@gmail.com>
2012-01-04 19:25:18 +0000

    Comments added to immfilter.m

--- STICS/stics4compile/immfilter.m
+++ STICS/stics4compile/immfilter.m
@@ -1,4 +1,14 @@
 function mobile = immfilter(movie);
+%IMMFILTER Immobile element removal filter.
+% Applies FFT and remove the zero-frequency component
+% from a movie.
+%
+% SYNOPSIS: mobile_movie = immfilter(movie);
+%
+% INPUT: movie - 3D array (x,y,t)
+% OUTPUT: mobile - movie with immobile component removed
+%
+% smg@lcbb.mit
 
 set(gcbf,'pointer','watch');
 h = waitbar(0,'Fourier Filtering Immobile Population...');
