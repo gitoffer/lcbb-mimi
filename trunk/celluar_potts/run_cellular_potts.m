6123e033a58b48dd84b65272dd7dc2dc500ef550
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2012-01-31 02:50:14 +0000

    working on cellular_potts

--- trunk/celluar_potts/run_cellular_potts.m
+++ trunk/celluar_potts/run_cellular_potts.m
@@ -1,30 +1,35 @@
 
 initial_lattice = ...
     [[1 1 1 2 2 2 3 3 3]; ...
+    [[1 1 1 2 2 2 3 3 3]; ...
+    [[1 1 1 2 2 2 3 3 3]; ...
     [4 4 4 5 5 5 6 6 6 ]; ...
+    [4 4 4 5 5 5 6 6 6 ]; ...
+    [4 4 4 5 5 5 6 6 6 ]; ...
+    [7 7 7 8 8 8 9 9 9 ]];
+    [7 7 7 8 8 8 9 9 9 ]];
     [7 7 7 8 8 8 9 9 9 ]];
 
 identity = [1 2 1 2 1 2 1 2 1];
 
 p.energy_function = @differential_adhesion;
-p.energy_parameters = [1 0 5];
-p.simulation_tempereature = 100;
+p.energy_parameters = [1 0 10 9 0 12];
+p.energy_parameters(6) = 2*sqrt(pi*p.energy_parameters(4));
+p.simulation_temperature = 100;
 p.identity = identity;
 
-max_time = 100;
+max_time = 1000;
 
 lattice = initial_lattice;
 % Actually this probably doesn't matter... set ground state energy to 0?
 %energy = feval(lattice,p);
 energy = 0;
 
-lattice_history = zeros(size(lattice),max_time);
+lattice_history = zeros([size(lattice),max_time]);
 energy_history = zeros(max_time);
 
 for i = 1:max_time
-				lattice_history(:,:,i) = lattice;
-				energy_history(i) = energy;
+    lattice_history(:,:,i) = lattice;
+    energy_history(i) = energy;
     [lattice,energy] = potts_step(lattice,energy,p);
 end
-
-
