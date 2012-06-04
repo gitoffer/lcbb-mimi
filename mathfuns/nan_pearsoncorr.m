13c4983b16ad48913cd0fce038f653b11d0f12cd
xie.shicong <xie.shicong@gmail.com>
2012-06-04 21:58:15 +0000

    Reverting previous commit, deleted a bunch of stuff

--- mathfuns/nan_pearsoncorr.m
+++ mathfuns/nan_pearsoncorr.m
@@ -1,4 +1,4 @@
-function corrcoef = nan_pearsoncorr(a,b,lag)
+function corrcoef = nan_pearsoncorr(a,b)
 %NANCORRCOEF Calculates the Pearson correlation coefficient for vectors A
 %and B that has NaNs. Only supported for 1D vectors and 2 inputs.
 %
@@ -6,15 +6,11 @@ function corrcoef = nan_pearsoncorr(a,b,lag)
 %
 % xies@mit.edu March 2012.
 
-[N,T] = size(b);
-if nargin < 3, lag = 0; end
-
-a = a(:,max(1,1-lag):min(T,T-lag));
-B = b(:,max(1,1+lag):min(T,T+lag));
+N = size(b,1);
 
 corrcoef = zeros(1,N);
 for i = 1:N
-    covMat = nancov(a,B(i,:));
+    covMat = nancov(a,b(i,:));
     variances = diag(covMat);
     covMat = covMat./sqrt(variances*variances');
     corrcoef(i) = covMat(1,2);
