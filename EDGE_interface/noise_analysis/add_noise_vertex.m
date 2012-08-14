bf0b9322b91a6713983f47671754fe5eea8e57f4
xie.shicong <xie.shicong@gmail.com>
2012-08-14 21:09:45 +0000

    Added get_cell_area.m as well as add_noise_vertex.m

---
+++ EDGE_interface/noise_analysis/add_noise_vertex.m
@@ -0,0 +1,18 @@
+function [delta_area,new_area] = add_noise_vertex(vx,vy,noise_level)
+
+if any(size(vx) ~= size(vy)), error('Size of vertices must be the same.'); end
+% N = size(vx);
+
+old_area = get_cell_area(vx,vy);
+
+% Add random noise to vertex positions
+
+vx = round(vx + randn(size(vx))*noise_level);
+vy = round(vy + randn(size(vy))*noise_level);
+
+new_area = get_cell_area(vx,vy);
+
+delta_area = new_area - old_area;
+
+end
+
