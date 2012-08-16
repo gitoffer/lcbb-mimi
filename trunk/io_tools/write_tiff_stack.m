a5c0661530160a9ed1c58a16864d018a7724a2ad
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2012-08-16 18:49:44 +0000

    Hopefully no conflicts

--- trunk/io_tools/write_tiff_stack.m
+++ trunk/io_tools/write_tiff_stack.m
@@ -2,8 +2,13 @@ function write_tiff_stack(imstack,filename)
 %WRITE_TIF_STACK Write a 3D image file as a TIF stack.
 %
 % SYNOPSIS: write_stuff_stack(imstack,filename)
+%  nb: filename must end in .tif
 %
 % xies@mit.edu
+
+[~, ~, ext] = fileparts(filename);
+
+if ~strcmpi(ext,'tif'), error('Please provide a .TIF file name.'); end
 
 [X,Y,N,M,L] = size(imstack);
 imstack = reshape(imstack,X,Y,N*M*L);
@@ -17,4 +22,6 @@ for i = 1:N*M*L
         imwrite(uint8(imstack(:,:,i)),filename,'tif', ...
             'compression','none','writemode','append');
     end
+end
+
 end
