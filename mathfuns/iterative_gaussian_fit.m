23a6f94ffcb9f5c2f3900c95c582fef80d3179ed
xie.shicong <xie.shicong@gmail.com>
2012-09-06 02:05:04 +0000

    Updated iterative_gaussian_fit.m

--- mathfuns/iterative_gaussian_fit.m
+++ mathfuns/iterative_gaussian_fit.m
@@ -1,4 +1,4 @@
-function parameters = iterative_gaussian_fit(curve,x,alpha,lb,ub)
+function parameters = iterative_gaussian_fit(y,x,alpha,lb,ub)
 %ITERATIVE_GAUSSIAN_FIT Uses LSQCURVEFIT to fit multiple Gaussians to a 1D
 %signal. Will use F-test to penalize for over-fitting.
 %
@@ -8,7 +8,7 @@ function parameters = iterative_gaussian_fit(curve,x,alpha,lb,ub)
 %
 % xies@mit. Feb 2012.
 
-if numel(x) ~= numel(curve)
+if numel(x) ~= numel(y)
     error('The input vector and the input curve should have the same dimensions');
 end
 
@@ -19,13 +19,18 @@ switch nargin
         lb = []; ub = [];
 end
 
-T = length(curve);
+T = length(y);
+old_fit = zeros(size(y));
+curve = y;
 
+% Initialize
 [height,max] = extrema(curve);
-guess = [height(1) max(1) 2]; % initial guesss
+guess = [height(1) x(max(1)) x(3)-x(1)]; % initial guesss
 significant = 1;
-S_null = Inf;
+S_null = sum(curve.^2);
 heights = []; peaks = []; vars = [];
+parameters = [];
+
 n_peaks = 0;
 % Suppress display
 opt = optimset('Display','off');
@@ -33,29 +38,47 @@ opt = optimset('Display','off');
 % While significant by F-test, fit 1 more gaussian
 while significant
 
-    [p,resnorm,residual] = lsqcurvefit(@lsq_gauss1d,guess,x,curve,lb,ub,opt);
-    S_alt = resnorm/(T-2*(n_peaks+1));
+    p = lsqcurvefit(@lsq_gauss1d,guess,x,curve,lb,ub,opt);
+    this_fit = lsq_gauss1d(p,x);
+    
+    residual = this_fit + old_fit - y;
+    resnorm = norm(residual);
+    
+    n_peaks = n_peaks + 1;
+    S_alt = resnorm/(T-3*(n_peaks));
     test_obs = S_alt/S_null;
     
-    P = fcdf(test_obs,T-(n_peaks+1)*3,T-n_peaks*3);
-    if P < alpha && any(residual < 0)
-        % The new 'curve to fit' is the rectified residual
-        residual(residual>0) = 0;
-        curve = -residual;
-        [height,max] = extrema(curve);
-        guess = [height(1) max(1) 2];
+    P = fcdf(test_obs,T-(n_peaks)*3,T-(n_peaks-1)*3);
+    if P < alpha
+        % Collect the "significant" parameters
+%         parameters(1,n_peaks) = p(1);
+%         parameters(2,n_peaks) = p(2);
+%         parameters(3,n_peaks) = p(3);
         heights = [heights p(1)];
         peaks = [peaks p(2)];
         vars = [vars p(3)];
+        
+        % Update the statistics
         significant = 1;
         S_null = S_alt;
-        n_peaks = n_peaks + 1;
+%         n_peaks = n_peaks + 1;
+        resnorm_old = resnorm;
+        old_fit = old_fit + this_fit;
+        
+        % The new 'curve to fit' is the residual
+%         residual(residual>0) = 0;
+        curve = -residual;
+        [height,max] = extrema(curve);
+        guess = [height(1) x(max(1)) x(3)-x(1)];
+        
     else
         significant = 0;
     end
     
 end
-% Collect output terms
-parameters(1,:) = heights(:);
-parameters(2,:) = peaks(:);
-parameters(3,:) = vars(:);
+
+parameters(1,:) = heights;
+parameters(2,:) = peaks;
+parameters(3,:) = vars;
+
+end
