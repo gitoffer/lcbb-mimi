3f1f6d5961324c895a8204eae149a7a625490c01
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2013-02-22 04:51:00 +0000

    Committed skeletal classes for +pulse package

--- trunk/MATLAB/scripts/visualize_cell.m
+++ trunk/MATLAB/scripts/visualize_cell.m
@@ -1,26 +1,45 @@
-ID = cellID;
+function visualize_cell(cells,ID,handle)
+%VISUALIZE_CELL Plots the raw area and myosin data for a given cell as well
+% as its fitted pulses, if applicable.
+%
+% USAGE: h = visualize_cell(cells,stackID)
+%
+% xies@mit.edu Feb 2013
 
-% if nargin > 1, h = varargin{1}; end
+% Extract cell
+this_cell = cells(ID);
 
-nonantime = ~isnan(master_time(IDs(ID).which).frame);
-time = master_time(IDs(ID).which).aligned_time;
-time = time(nonantime);
+nonan_frame = this_cell.dev_frame;
+nonan_frame = ~isnan(nonan_frame);
+time = this_cell.dev_time( nonan_frame );
 
-% cell_fits( 
+if nargin < 3, handle = gca; end
 
-% Plot raw data and fits
-h = plotyy(time,myosins_sm(nonantime,ID),...
-    time,areas_sm(nonantime,ID));
-set(h(2),'Xlim',[min(master_time(IDs(ID).which).aligned_time) ...
-    max(master_time(IDs(ID).which).aligned_time)]);
-hold on
-plot(cell_fits(ID).time,cell_fits(ID).signal,'c-')
-plot(cell_fits(ID).time,cell_fits(ID).bg,'r-')
-plot(cell_fits(ID).time,cell_fits(ID).signal + cell_fits(ID).bg,'k-');
-hold off
+% Plot raw data: myosin + area
+[h,fig1,fig2] = plotyy(handle, time, this_cell.myosin_sm(nonan_frame), ...
+    time, this_cell.area_sm(nonan_frame) );
 
-title(['Cell #' num2str(ID)])
-set(h(1),'Xlim',[min(master_time(IDs(ID).which).aligned_time) ...
-    max(master_time(IDs(ID).which).aligned_time)]);
+set(fig1,'Color','g'); set(fig2,'Color','k')
 
-% end
+% set x-axis limits
+set(h(2) , 'Xlim', [min(time) max(time)] );
+
+if this_cell.flag_fitted
+    
+    hold(handle,'on');
+    
+    plot(handle,this_cell.fit_time, this_cell.fit_colorized);
+    plot(handle,this_cell.fit_time, this_cell.fit_bg, 'c-');
+    plot(handle,this_cell.fit_time, this_cell.fit_curve, 'm-');
+    
+    hold(handle,'off');
+    
+    title(handle,['EDGE #' num2str(this_cell.cellID)])
+    
+end
+
+set(handle,'Xlim',[min(this_cell.dev_time) max(this_cell.dev_time)]);
+
+% if nargout > 0, varargout{1} = h; end
+
+end
