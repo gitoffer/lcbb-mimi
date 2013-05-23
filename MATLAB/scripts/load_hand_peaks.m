bf0926e7ec74038f349980e4f0573ec78fbe9774
xie.shicong <xie.shicong@gmail.com>
2013-05-23 17:42:33 +0000

    Deleted scripts/draw_embryo_script.m and scripts/load_hand_peaks.m

--- MATLAB/scripts/load_hand_peaks.m
+++
@@ -1,9 +0,0 @@
-file = '~/Documents/MATLAB/EDGE/DATA_OUTPUT/2color_4 013012/';
-
-peaks_hand = zeros(num_frames,num_cells);
-
-for i = 2:num_frames
-    data = load([file num2str(i)]);
-    peaks_hand(i,data.cell_indices) = 1;
-end
-
