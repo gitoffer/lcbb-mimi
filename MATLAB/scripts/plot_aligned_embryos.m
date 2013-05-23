24b578491f5a3a4176d8a4261ed0857f11df118c
xie.shicong <xie.shicong@gmail.com>
2013-05-23 17:38:53 +0000

    Some scripts added

--- MATLAB/scripts/plot_aligned_embryos.m
+++ MATLAB/scripts/plot_aligned_embryos.m
@@ -1,25 +1,28 @@
 %align_embryos
 
 num_embryos = numel(input) - 2;
-name2plot = 'myosin_sm';
+name2plot = 'area_sm';
 
 figure
 color = hsv(num_embryos);
-
-for i = 1:num_embryos
+embryoID_OI = [8:9];
+for i = 1:numel( embryoID_OI )
     
-    time = embryo_stack(i).dev_time;
+    embryoID = embryoID_OI(i);
+    
+    time = embryo_stack(embryoID).dev_time;
     
     H(i) = shadedErrorBar(time, ...
-        nanmean(embryo_stack(i).(name2plot),2), ...
-        nanstd(embryo_stack(i).(name2plot),[],2), ...
+        nanmean(embryo_stack(embryoID).(name2plot),2), ...
+        nanstd(embryo_stack(embryoID).(name2plot),[],2), ...
         {'color',color(i,:)},1);
     
-    entries{i} = ['Embryo ' num2str(i) ', ' num2str(input(i).dt) ' sec/frame'];
+    entries{i} = ['Embryo ' num2str(embryoID) ', ' num2str(input(embryoID).dt) ' sec/frame'];
     
     hold on
     
 end
+
 hold off
 xlabel('Time (sec)')
 legend([H.mainLine],entries)
