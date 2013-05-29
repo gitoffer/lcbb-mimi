fd215f4948c697dc335a2f8e8d0a7c2d697c292b
xie.shicong <xie.shicong@gmail.com>
2013-05-29 18:21:10 +0000

    Commited /Elena KDE and PCLC functions

--- MATLAB/lsq_functions/lsq_gauss2d.m
+++ MATLAB/lsq_functions/lsq_gauss2d.m
@@ -1,6 +1,6 @@
-function y = lsq_gauss1d(params,x)
+function y = lsq_gauss2d(params,x)
 %Intended for use with the OPTIMIZATION TOOLBOX, especiallu LSQCURVEFIT.
-% SYNOPSIS: y = lsq_gauss1d(params,x)
+% SYNOPSIS: y = lsq_gauss2d(params,x)
 % INPUT: params: [Amplitude mean standard_deviation];
 %        x - the "domain"
 
@@ -8,7 +8,8 @@ function y = lsq_gauss1d(params,x)
 A = params(1);
 mu = params(2);
 sigma = params(3);
-y = x*A.*exp(-(x-mu).^2/(2*sigma^2));
+y = (1/sigma^2)*(mu-x)*A.*exp(-((mu- x).^2)/(2*sigma^2));
+
 
 end
 
--- MATLAB/lsq_functions/lsq_gaussderiv.m
+++ MATLAB/lsq_functions/lsq_gaussderiv.m
@@ -1,6 +1,6 @@
-function y = lsq_gauss1d(params,x)
+function y = lsq_gaussderiv(params,x)
 %Intended for use with the OPTIMIZATION TOOLBOX, especiallu LSQCURVEFIT.
-% SYNOPSIS: y = lsq_gauss1d(params,x)
+% SYNOPSIS: y = lsq_gaussderiv(params,x)
 % INPUT: params: [Amplitude mean standard_deviation];
 %        x - the "domain"
 
@@ -8,7 +8,7 @@ function y = lsq_gauss1d(params,x)
 A = params(1);
 mu = params(2);
 sigma = params(3);
-y = x*A.*exp(-(x-mu).^2/(2*sigma^2));
+y = A.*(x.^2 - 1).*exp(-(x-mu).^2/(2*sigma^2));
 
 end
 
