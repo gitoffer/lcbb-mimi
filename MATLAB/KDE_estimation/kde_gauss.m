d24bd55abebd678d6e7ea76b5c3c5aad61d11e1a
xie.shicong <xie.shicong@gmail.com>
2013-06-18 21:05:56 +0000

    Added KDE.m (diffusion estimator), count_modes_gaussian, kde_gauss

--- MATLAB/KDE_estimation/kde_gauss.m
+++ MATLAB/KDE_estimation/kde_gauss.m
@@ -1,29 +1,30 @@
-function [kde_est,kde_bins,tbins,Ninslice,Nzeros,hout] = kde_gauss_temporal_binning(embryo_stack,kde_bins,dt,h)
-%KDE_GAUSS_TEMPORAL_BINNING cta-specific wrapper for KSDENSITY or KDE
+function [kde_est,kde_bins,Nzeros,hout] = kde_gauss(embryo_stack,kde_bins,sliceID,h,dataname)
+%KDE_GAUSS cta-specific wrapper for KSDENSITY or KDE, for use with
+% TEMPORAL_SLICE to produce a series of KDEs
 %
 % USAGE:
-% [est,est_bins,tbins,Ninslice,Nzeros,h] = kde_gauss_temporal_binning(data,kde_params,temp_params)
+% [est,est_bins,tbins,Ninslice,Nzeros,h] = kde_gauss
 %
 % INPUT: embryo_stack,
 %        kde_bins - If vector: A vector of the KDE evaluation bins
 %                   If number of bins is not power of 2, will round up.
 %                   If number: the number of KDE bins, from min to max.
 %                   Will round to next power of 2.
-%        dt - A 1xNembryo vector of time bins for temporal slicing (sec)
-%        h - kernel size (Default, uses diffusion estimator from KDE.m)
+%        sliceID - Output of TEMPORAL_SLICING.m
+%        h - kernel size (If not defined, uses diffusion estimator from KDE.m)
+%        dataname - defualt = 'area'
 %
 % OUTPUT: KDE_EST - estimate
 %         KDE_BINS - evaluation points
-%         tbins - left edges of temporal slices
-%         Ninbins - number of data points in
 %         Nzeros - number of zero crossing for derivative
 %         h - kernel size used
 %
-% SEE ALSO: KSDENSITY, CTA_KDE_AREA, KDE
+% SEE ALSO: KSDENSITY, CTA_KDE_AREA, KDE, TEMPORALLY_SLICE
 %
 % xies@mit.edu
 
-data = cat(2,embryo_stack.area);
+if nargin < 5, dataname = 'area'; end
+data = cat(2,embryo_stack.(dataname) );
 
 % -- Generate KDE bins --
 if isscalar(kde_bins)
@@ -37,21 +38,23 @@ else
 end
 
 % -- KDE ---
-kde_est = zeros(numel(tbin_edges) - 1, numel(kde_bins));
-Nzeros = zeros( 1, numel(tbin_edges) - 1);
-Ninslice = zeros( 1, numel(tbin_edges) - 1);
-for i = 1:numel( tbin_edges ) - 1
+ntbins = numel(unique(sliceID)) - 1;
+kde_est = zeros(ntbins, numel(kde_bins));
+Nzeros = zeros( 1, ntbins);
+Ninslice = zeros( 1, ntbins);
+for i = 1:ntbins
     
-    data_within_slice = nonans( ...
-        data( sliceID == i ) );
+    data_within_slice = nonans( data( sliceID == i ) );
     
-    % RUN KSDENSITY on non-empty sets
+    % RUN KSDENSITY on non-empty sets, if h is given
     if ~isempty(data_within_slice)
+        
         if nargin == 4
             kde_est(i,:) = ksdensity(data_within_slice, kde_bins, ...
                 'Width', h);
             hout = h;
         else
+            % OR run KDE if h not given
             [h,kde_est(i,:),kde_bins] = kde(data_within_slice, ...
                 numel(kde_bins), min(kde_bins), max(kde_bins) );
             if nargout == 6
@@ -59,13 +62,10 @@ for i = 1:numel( tbin_edges ) - 1
             end
         end
         Ninslice(i) = numel(sliceID(sliceID == i));
-        ind = crossing( ...
-            gaussian_derivative(h,data_within_slice,kde_bins) );
-        Nzeros(i) = numel(ind);
+        Nzeros(i) = count_modes_gaussian(h,data_within_slice,kde_bins);
+        
     end
     
 end
-
-tbins = [min_time tbin_edges( 2 : end - 1 )];
 
 end
