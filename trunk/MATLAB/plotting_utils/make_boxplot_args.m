6e4bf003f8a811bee74f9c76d8e42d9a664f3f7c
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2013-07-10 21:48:37 +0000

    custom label input

--- trunk/MATLAB/plotting_utils/make_boxplot_args.m
+++ trunk/MATLAB/plotting_utils/make_boxplot_args.m
@@ -3,19 +3,40 @@ function [X,G] = make_boxplot_args(varargin)
 % contatenate all inputs and make a corresponding grouping array.
 %
 % USAGE: [X,G] = make_boxplot_args(A,B,C,...)
+%        [X,G] = make_boxplot_args(A,B,C,...,labels)
+%
+% If the last argument is a cell array of strings, it will be used to
+% generate G (labels).
 %
 % xies@mit.edu
 
 if nargin < 1, error('No argument!'); end
+% check for last argument
+if iscellstr(varargin{end})
+    RELABEL = 1;
+    labels = varargin{end}; varargin(end) = [];
+else
+    RELABEL = 0;
+end
 
 flat = @(x) x(:);
 arg_array = varargin;
 arg_array = cellfun(flat,arg_array,'UniformOutput',false);
 X = cat(1,arg_array{:});
 
+% make labels
 G = cell(numel(X),1);
 num_elements = cellfun(@numel,arg_array);
 num_talley = [1 cumsum(num_elements)];
 for i = 1:numel(num_elements)
-    [G{num_talley(i):num_talley(i+1)}] = deal(num2str(i));
+    
+    if RELABEL
+        label = labels{i};
+    else
+        label = num2str(i);
+    end
+    [G{num_talley(i):num_talley(i+1)}] = deal( label );
+    
+end
+
 end
