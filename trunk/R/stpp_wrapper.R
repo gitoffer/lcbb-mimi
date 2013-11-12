67d603a3f109cd861270fbcb956bb7ff44b4a464
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2013-11-12 17:25:13 +0000

    Data-frame centric loading/accessing

--- trunk/R/stpp_wrapper.R
+++ trunk/R/stpp_wrapper.R
@@ -1,10 +1,10 @@
 # get_PCFhat_stpp - Wrapper for PCFhat functionality in STPP
 
-get_PCFhat_stpp <- function( xyt, s.region=NULL, t.region=NULL, u, v,
-							h=hmsemin, label=NULL) {
+get_PCFhat_stpp <- function( xyt, s.region, t.region, u, v,
+							h=hmsemin ) {
 
 	# Load libraries
-	require("stpp")
+	library("stpp")
 	
 	# parse inputs and/or default behavior
 	# Use powers of 2 for FFT kernel estimation
@@ -23,36 +23,36 @@ get_PCFhat_stpp <- function( xyt, s.region=NULL, t.region=NULL, u, v,
 	mut = Mt$y[ findInterval( t, Mt$x) ] * dim(xyt)[1]
 	
 	# Estimate 2d spatial kernel density
-	# Optimize t?e kernel bandwidth (h) by mean squared-error (MSE)
+	# Optimize the kernel bandwidth (h) by mean squared-error (MSE)
 	#	analysis at set h values to scan
 	#	Default: number of h-values: 50, max h-value: 4
 	hmsemin = mse2d( as.points(xy), s.region, nsmse = 50, range = 10)
-	hmsemin = hmsemin$h[ which.min(hmsemin$mse) ] # Argmin MSE
+	hmsemin = hmsemin$hmsemin[ which.min(hmsemin$mse) ] # Argmin MSE
+	print(h)
 	
 	Ms = kernel2d( as.points(xy), s.region, h=h, nx=nx, ny=ny)
 	atx = findInterval( x=xyt[,1], vec = Ms$x)
 	aty = findInterval( x=xyt[,2], vec = Ms$y)
 	mhat = NULL
-	for(i in 1:length(atx)) {
-		mhat = c(mhat, Ms$z[atx[i],aty[i]])
-	}
+	for(i in 1:length(atx)) mhat = c(mhat, Ms$z[atx[i],aty[i]])
 	
-	g = PCFhat_label(xyt=xyt,
-				lambda = mhat*mut/ dim(xyt)[1],
-				dist = u, times = v,
+	g = PCFhat(xyt=xyt,
+			   lambda = mhat*mut/ dim(xyt)[1],
+			   dist = u, times = v,
 				s.region = s.region, t.region = t.region,
-				correction=TRUE, hs = h,label=label)
-		
+				correction=TRUE)
+	
 	return(g)
 
 }
+
 
 # get_STIKhat_stpp - Wrapper for STIKhat functionality in STPP
 
 get_STIKhat_stpp <- function( xyt, s.region, t.region, u, v) {
 	
 	# Load libraries
-	require("stpp")
+	library("stpp")
 	
 	# Kernel estimation sizes
 	nt = 2^8
