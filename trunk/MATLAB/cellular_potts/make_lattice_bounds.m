e5703483ec346e5d91b6a7115c8ab049524b445d
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2014-04-04 00:05:05 +0000

    Deleted cellular potts - incomplete and useless!

--- trunk/MATLAB/cellular_potts/make_lattice_bounds.m
+++
@@ -1,8 +0,0 @@
-function bounded_lattice = make_lattice_bounds(lattice,method,size)
-
-switch method
-    case 'zero'
-        bounded_lattice = padarray(lattice,[size size],0);
-    otherwise
-        error('Unsupported boundary condition.');
-end
